5c74dd4a3780922675e55f702cb216e55bcacee9
Arda Can Aras <ardaaras99@gmail.com>
2024-05-17 13:53:47 +0300

    removed unnecassary file structure

--- baseline_text_graphs/__init__.py
+++ baseline_text_graphs/__init__.py
@@ -10,8 +10,8 @@ DATA_PATH = PROJECT_PATH.joinpath("data")
 DATA_PATH.mkdir(parents=True, exist_ok=True)
 print("Data path: ", DATA_PATH)
 
-TRANSFORMED_DATA_PATH = PROJECT_PATH.joinpath("data/transformed-data")
-TRANSFORMED_DATA_PATH.mkdir(parents=True, exist_ok=True)
+# TRANSFORMED_DATA_PATH = PROJECT_PATH.joinpath("data/transformed-data")
+# TRANSFORMED_DATA_PATH.mkdir(parents=True, exist_ok=True)
 
 FILE_ID = "19jy8pu4GVUKq7FvrscmitmTUZl3eI1Wa"
 DOWNLOAD_LINK = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
